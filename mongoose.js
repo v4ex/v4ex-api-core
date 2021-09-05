09a3b3dbf4ab84eb5e5c602c80bda6e7d534b199
Xuwei Li <dev@xuwei.li>
2021-09-05 06:17:14 +0000

    * Add 'CORE' prefix to .env. * Upgrade mongoose to 6.0.4.

--- mongoose.js
+++ mongoose.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.CORE_MONGO_URI)
 
 module.exports = mongoose
